f4a1092d5bc1b5f7858e0cf5406d280576079eef
Carter Nosek <carternose@gmail.com>
2024-10-23 15:32:08 -0400

    added rudimentary navbar

---
+++ app/src/components/navbar/index.js
@@ -0,0 +1,45 @@
+// https://www.geeksforgeeks.org/create-a-responsive-navbar-using-reactjs/
+
+import React from "react";
+import {
+    Nav,
+    NavLink,
+    Bars,
+    NavMenu,
+    NavBtn,
+    NavBtnLink,
+} from "./elements";
+
+function Navbar() {
+    return (
+        <>
+            <Nav>
+                <Bars />
+                <NavMenu>
+                    <NavLink to="/" >
+                        Home
+                    </NavLink>
+                    <NavLink to="/dashboard" activeStyle>
+                        Dashboard
+                    </NavLink>
+                    <NavLink to="/calendar" activeStyle>
+                        Calendar
+                    </NavLink>
+                    <NavLink to="/profile" activeStyle>
+                        Profile
+                    </NavLink>
+                    <NavLink to="/sign-up" activeStyle>
+                        Sign Up
+                    </NavLink>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to="/signin">
+                        Sign In
+                    </NavBtnLink>
+                </NavBtn>
+            </Nav>
+        </>
+    );
+};
+
+export default Navbar;
