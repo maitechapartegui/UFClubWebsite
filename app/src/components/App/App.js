d311357742badd46740ea0d4635239ddb341984d
Carter Nosek <carternose@gmail.com>
2024-10-25 11:47:03 -0400

    got private routes working

--- app/src/components/App/App.js
+++ app/src/components/App/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from '../navbar';
 import Dashboard from '../../pages/Dashboard/Dashboard';
 import Calendar from '../../pages/Calendar/Calendar';
 import Profile from '../../pages/Profile/Profile';
-import SignUp from '../../pages/Sign-Up/sign-up';
-import Login from '../Login/Login';
 import useToken from './useToken';
 import Home from '../../pages';
 import PrivateRoutes from './privateRoutes'
@@ -14,17 +12,13 @@ import PrivateRoutes from './privateRoutes'
 function App() {
   const {token, setToken} = useToken()
 
-  if(!token) {
-    return <Login setToken={setToken} />
-  }
-
   return (
       <BrowserRouter>
       <Navbar />
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/calendar" element={<Calendar />}/>
-          <Route element={<PrivateRoutes />}>
+          <Route element={<PrivateRoutes token={token} setToken={setToken}/>}>
             <Route path="/dashboard" element={<Dashboard />}/>
             <Route path="/profile" element={<Profile />}/>
           </Route>
--- app/src/components/App/privateRoutes.js
+++ app/src/components/App/privateRoutes.js
@@ -1,8 +1,10 @@
-import { Navigate, Outlet } from 'react-router-dom'
+import {Outlet} from 'react-router-dom'
 import Login from '../Login/Login'
 
 const PrivateRoutes = ({token, setToken}) => {
 return (
-    token ? <Outlet/> : <Login setToken={setToken} />
+    token === true ? <Outlet/> : <Login setToken={setToken} />
   )
 }
+
+export default PrivateRoutes
